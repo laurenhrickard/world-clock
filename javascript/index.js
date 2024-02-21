4dfd4972e24266beecde90782d96e426f0e04c5c
laurenrickard13 <153468324+laurenhrickard@users.noreply.github.com>
2024-02-21 12:04:21 -0800

    Added current location to select HTML and JavaScript functionality

--- javascript/index.js
+++ javascript/index.js
@@ -37,6 +37,9 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimezone = event.target.value;
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
 
